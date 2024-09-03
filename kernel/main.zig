a4f3026032a77040e72eb45f2066c48605bfe42d
YiraSan <45128167+YiraSan@users.noreply.github.com>
2024-09-03 12:34:23 +0200

    add scope

--- kernel/main.zig
+++ kernel/main.zig
@@ -2,8 +2,10 @@ const std = @import("std");
 const arch = @import("arch/arch.zig");
 const build_options = @import("build_options");
 
+const log = std.log.scoped(.main);
+
 pub fn main() !void {
     try arch.init();
 
-    std.log.info("violet v{s}", .{build_options.version});
+    log.info("violet v{s}", .{build_options.version});
 }
