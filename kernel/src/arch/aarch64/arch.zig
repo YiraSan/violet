3e5b8aaab93cfddd7c257560461d156fcd234a61
YiraSan <45128167+YiraSan@users.noreply.github.com>
2025-01-02 16:49:45 +0100

    init arch

---
+++ kernel/src/arch/aarch64/arch.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+fn currentEL() u64 {
+    var current_el: u64 = 0;
+     
+    asm volatile (
+        "mrs %[result], CurrentEL"
+        : [result] "=r" (current_el)
+        : 
+        : "memory"
+    );
+
+    current_el = (current_el >> 2) & 0x3;
+
+    return current_el;
+}
+
+pub fn init() void {
+    const current_el = currentEL();
+    if (current_el != 1) {
+        std.log.err("execution level {}", .{current_el});
+        unreachable;
+    } else {
+        std.log.debug("execution level {}", .{current_el});
+    }
+}
---
+++ kernel/src/arch/arch.zig
@@ -0,0 +1,17 @@
+const builtin = @import("builtin");
+
+pub const init = switch (builtin.cpu.arch) {
+    .aarch64 => @import("aarch64/arch.zig").init,
+    else => unreachable,
+};
+
+pub fn halt() noreturn {
+    while (true) {
+        if (comptime builtin.cpu.arch == .x86_64) {
+            asm volatile ("hlt");
+        } else if (comptime builtin.cpu.arch == .aarch64 or builtin.cpu.arch == .riscv64) {
+            asm volatile ("wfi");
+        }
+    }
+    unreachable;
+}
