9ab74d486e1926222c69f42c0539b6502acacf8f
YiraSan <45128167+YiraSan@users.noreply.github.com>
2025-12-16 15:24:33 +0100

    clean genesis main

--- genesis/src/main.zig
+++ genesis/src/main.zig
@@ -12,14 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const builtin = @import("builtin");
-const basalt = @import("basalt");
-
-pub fn main() !void {
-    switch (builtin.cpu.arch) {
-        .aarch64 => {
-            asm volatile ("svc #0");
-        },
-        else => unreachable,
-    }
-}
+pub fn main() !void {}
