ac9a24bea0e177fca63f9450d9290236c0346067
YiraSan <45128167+YiraSan@users.noreply.github.com>
2026-08-29 21:46:08 +0200

    check memory requirement

--- kernel/src/boot/root.zig
+++ kernel/src/boot/root.zig
@@ -106,5 +106,9 @@ export fn kernel_entry() noreturn {
     kernel.serial.clear(null);
     std.log.info("version {s}", .{build_options.version});
 
+    const available_mib = mem.phys.availablePages() * mem.paging.page_size / 1024 / 1024;
+    if (available_mib < 384) std.debug.panic("available memory: {} MiB is under 384 MiB requirement", .{available_mib});
+    std.log.info("available memory: {} MiB", .{available_mib});
+
     cpu.halt();
 }
